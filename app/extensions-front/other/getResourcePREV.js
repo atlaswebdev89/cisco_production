0fd2066c58b3289580ab426583b97dcd7479f84c
Atlas <doroshuk33@yandex.by>
2021-07-28 15:11:12 +0300

    update

--- app/extensions-front/other/getResourcePREV.js
+++ app/extensions-front/other/getResourcePREV.js
@@ -1,32 +1,32 @@
 (function($){
     "use strict";
     //uri для запросов
-    var uri = '/storage/getResourse/';
+    let uri = '/storage/getResourse/';
     //Счетчик для ограничения ajax запросов
-    var count = 3;
-    var path = '';
-    var controlls = $('.control-panel').parent().html();
+    let count = 3;
+    let path = '';
     //Для динамических элементов страницы (добавленных черех ajax или append)
     //используем делегированную обработку событий
     $('.diskYandex').on('click', "span.arrow-collapse", function (e) {
+        console.log(e.target);
         var elemClick = $(this).attr('id');
         var elem = $(this);
         var icon = $(this).find('i');
-        var collapse = elem.closest('li').children('.dynamic').children('div').children('.collapse');      
-         
+              
             if($(this).hasClass('active')) {
                         elem.removeClass('active');
                     icon.removeClass().addClass('icon-folder2');
                 }
-            
-            collapse.each(function (e) {
+                
+            elem.parent('li').children('.collapse').each(function (e) {
                     if($(this).find('li').length > 0) {
                             $(this).collapse('toggle');
+                            
                     }else {
                             if (elem.data("type") == "dir" && count != 0 && path != elem.data('path')) {
                                 path = elem.data('path');
                                     count--;
-                                 ajaxDataTransfer('path=' + elem.data('path'), elem, collapse, controlls);
+                                 ajaxDataTransfer('path=' + elem.data('path'), elem);
                              } 
                     }
 
@@ -37,7 +37,7 @@
             })
     });         
   /* jquery function transmission data on form*/
-  function ajaxDataTransfer (path, elem, collapse, controlls) {
+  function ajaxDataTransfer (path, elem) {
         $.ajax ({
             type: 'POST',
             url:uri,
@@ -46,37 +46,39 @@
             dataType: "json",
             beforeSend: function (data) {
                 //Блокируем кнопку и элементы формы
-                elem.append('  <i class="ajax-loader icon-folder-download"></i>');
             },
             success:  function (data) {
-                elem.find('.ajax-loader').remove();
                 if(data) {
                     if(data.status == true){
+                            let divElem = elem.closest('li').children('.collapse');
                             //Очистка елемента
-                            collapse.empty();
+                            divElem.empty();
                                     $.each(data.data, function (index, value){
                                         if(value.type == "dir") {
-                                                    var divList = "<div class='row dynamic'><div class='col-md-12'><ul class='collapse pl-25'></ul></div></div>";
+                                                    var divList = "<ul class='collapse pl-25'></ul>";
                                                     var icon = "<i class='icon-folder2'></i>";
-                                                        collapse.append("<li><div class='row no-pad static'><div class='col-md-6 text-left'><span id='"+value.resourse_id +"' class = 'arrow-collapse  menu-expand'  data-path ='"+value.path+"' data-type = '"+value.type+"'>" +icon+ " "  +value.name+"</span></div><div class='col-md-3'>"+controlls+"</div><div class='col-md-3 text-right'><span >  " +value.date+" | "+ value.time+"</span></div></div>"+divList+"</li>");
+                                                        divElem.append("<li><span id='"+value.resourse_id +"' class = 'arrow-collapse collapsed menu-expand'  data-path ='"+value.path+"' data-type = '"+value.type+"'>" +icon+ " "  +value.name+"</span><span class='text-right'>  (" +value.date+" | "+ value.time+")</span>"+divList+"</li>");
                                                 }else if (value.type == "file") {
                                                     var icon = "<i class='icon-files'></i>";
-                                                        collapse.append("<li><div class='row no-pad static'><div class='col-md-6 text-left'><span id='"+value.resourse_id +"' class = 'arrow-collapse collapsed menu-expand'  data-path ='"+value.path+"' data-type = '"+value.type+"'>" +icon+ " "  +value.name+"</span></div><div class='col-md-3'>"+controlls+"</div><div class='col-md-3 text-right'><span class='text-right'>  " +value.date+" | "+ value.time+"</span></div></div></li>");
+                                                        divElem.append("<li><span id='"+value.resourse_id +"' class = 'arrow-collapse collapsed menu-expand'  data-path ='"+value.path+"' data-type = '"+value.type+"'>" +icon+ " "  +value.name+"</span><span class='text-right'>  (" +value.date+" | "+ value.time+")</span></li>");
                                                 }
                                     })
-                                collapse.collapse('show');
+                                divElem.collapse('show');
                             count = 3;
                     }else {
                         if (data.status == false) {
-                                    collapse.empty().append("<li><i class='icon-file-empty2'></i> Папка пустая...</li>");
-                                collapse.collapse('show');
+                                let divElem = elem.closest('li').children('.collapse');
+                                    divElem.empty().append("<li><i class='icon-file-empty2'></i> Папка пустая...</li>");
+                                divElem.collapse('show');
                             count = 3;
                         }
                     }
                 }
             },
             error: function(x, t, e){
-                if( t === 'timeout') {}
+                if( t === 'timeout') {
+                   
+                }
             }
         })
     }          
--- app/templates/template_storage_yandex.php
+++ app/templates/template_storage_yandex.php
@@ -5,29 +5,51 @@
                                                 <h2 class="dark text-center" style="" data-aos="zoom-in" data-aos-easing="none" data-aos-duration="500" data-aos-delay="0"><strong>Access Point</strong>&nbsp; CiscoWifi</h2>
                                                 <p class="compressed-box-50 mb-100 dark text-center" style="">Статистика установленных точек доступа CiscoWifi</p>
 
-                                            <div class="compressed-box-75 text-left dark">
+                                            <div class="container text-left dark">
                                                {% if (listDisk) %}
                                                     <ul class="diskYandex">
                                                         {% for data in listDisk %}
                                                          <li>
-                                                             <span id = {{data.resourse_id}} class = "arrow-collapse  menu-expand"  data-path ="{{data.path}}" data-type = "{{data.type}}">
-                                                                 {% if data.type == 'dir'%}
-                                                                    <i class="icon-folder2"></i>
-                                                                 {% elseif data.type == 'file' %}
-                                                                    <i class="icon-files"></i>
-                                                                 {% endif %}
-                                                                    {{data.name}}
-                                                             </span>
-                                                             
-                                                             <span class="text-right"> ({{data.date}} | {{data.time}})</span>
-
-                                                             {% if data.type == 'dir'%}
-                                                             <ul class="collapse pl-25" ></ul>
-                                                             {% endif %}
-                                                         </li>
-                                                         {% endfor %}
+                                                            <div class="row no-pad static">
+                                                                <div class="col-md-6 text-left">
+                                                                    <span id = {{data.resourse_id}} class = "arrow-collapse  menu-expand"  data-path ="{{data.path}}" data-type = "{{data.type}}">
+                                                                        {% if data.type == 'dir'%}
+                                                                           <i class="icon-folder2"></i>
+                                                                        {% elseif data.type == 'file' %}
+                                                                           <i class="icon-files"></i>
+                                                                        {% endif %}
+                                                                           {{data.name}}
+                                                                    </span>
+                                                                </div>
+                                                                <div class="col-md-3">
+                                                                    <span class="control-panel">
+                                                                        <i class="icon-download2"></i>
+                                                                        <i class="icon-upload2"></i>
+                                                                        <i class="icon-delete"></i>
+                                                                        <i class="icon-box-add"></i>
+                                                                        <i class="icon-loading"></i>
+                                                                        <i class="icon-tag"></i>
+                                                                    </span>
+                                                                </div>
+                                                                <div class="col-md-3 text-right">
+                                                                    <span>
+                                                                       {{data.time}} | {{data.date}} 
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            
+                                                            {% if data.type == 'dir'%}
+                                                                <div class="row dynamic">
+                                                                    <div class="col-md-12">
+                                                                        <ul class="collapse pl-25">
+                                                                               
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>   
+                                                            {% endif %}
+                                                        </li>
+                                                        {% endfor %}
                                                     </ul>
-                                                    
                                                {% endif %}
                                             </div>
                                                         
