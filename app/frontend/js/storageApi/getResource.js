453a9c4dc923094434168a2e8f4f805717914bd1
Atlas_WebDev <web.dev89@yandex.by>
2021-08-01 21:51:57 +0300

    controlls items

--- app/frontend/js/storageApi/getResource.js
+++ app/frontend/js/storageApi/getResource.js
@@ -1,12 +1,31 @@
 //Счетчик для ограничения ajax запросов
 var count_ajax = 3;
-var controlls = {
-       download:'icon-download2',
-       upload:'icon-upload2',
+
+var controlls_dir_items = {
+       download:    ['icon-download2',  "Скачать"],
+       upload:      ['icon-upload2',    "Загрузить"],
+       update:      ['icon-loading',    "Обновить"],
+       rename:      ['icon-tag',        "Переименовать"],
+       add:         ['icon-box-add',    "Создать папку"],
+       delete:      ['icon-delete',     "Удалить"]
     };
-    
-var controlls = $('.control-panel.dir').parent().html();
-var controlls_file = controlls;
+var controlls_file_items = {
+       download:    ['icon-download2',  "Скачать"],
+       rename:      ['icon-tag',        "Переименовать"],
+       delete:      ['icon-delete',     "Удалить"]
+    };
+
+var controlls_dir ='<span class="control-panel dir">';
+    for (let key in controlls_dir_items) {
+       controlls_dir +='<i class= "'+controlls_dir_items[key][0]+' controlls" data-target = "'+key+'" data-title="'+controlls_dir_items[key][1]+'"></i> ';
+    }
+controlls_dir +='</span>';
+
+var controlls_file ='<span class="control-panel file">';
+    for (let key in controlls_file_items) {
+       controlls_file +='<i class= "'+controlls_file_items[key][0]+' controlls" data-target = "'+key+'" data-title="'+controlls_file_items[key][1]+'"></i> ';
+    }
+controlls_file +='</span>';
 
 (function($){
     "use strict";
@@ -58,7 +77,7 @@ var controlls_file = controlls;
                                         if(value.type == "dir") {
                                                     var divList = "<div class='row dynamic'><div class='col-md-12'><ul class='collapse pl-25'></ul></div></div>";
                                                     var icon = "<i class='icon-folder2'></i>";
-                                                        collapse.append("<li><div class='row no-pad static'><div class='col-md-6 text-left'><span id='"+value.resourse_id +"' class = 'arrow-collapse  menu-expand'  data-path ='"+value.path+"' data-type = '"+value.type+"'>" +icon+ " "  +value.name+"</span></div><div class='col-md-3'>"+controlls+"</div><div class='col-md-3 text-right'><span >  "+ value.time+"  | " +value.date+"</span></div></div>"+divList+"</li>");
+                                                        collapse.append("<li><div class='row no-pad static'><div class='col-md-6 text-left'><span id='"+value.resourse_id +"' class = 'arrow-collapse  menu-expand'  data-path ='"+value.path+"' data-type = '"+value.type+"'>" +icon+ " "  +value.name+"</span></div><div class='col-md-3'>"+controlls_dir+"</div><div class='col-md-3 text-right'><span >  "+ value.time+"  | " +value.date+"</span></div></div>"+divList+"</li>");
                                                 }else if (value.type == "file") {
                                                     var icon = "<i class='icon-files'></i>";
                                                         collapse.append("<li><div class='row no-pad static'><div class='col-md-6 text-left'><span id='"+value.resourse_id +"' class = 'arrow-collapse collapsed menu-expand'  data-path ='"+value.path+"' data-type = '"+value.type+"'>" +icon+ " "  +value.name+"</span></div><div class='col-md-3'>"+controlls_file+"</div><div class='col-md-3 text-right'><span class='text-right'>  "+ value.time+"  | " +value.date+"</span></div></div></li>");
@@ -76,7 +95,5 @@ var controlls_file = controlls;
                 }
         } 
     });
-    
-    
 })(jQuery);
 
